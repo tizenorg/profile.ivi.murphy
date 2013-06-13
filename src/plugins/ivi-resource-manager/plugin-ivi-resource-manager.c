20a1ada83d4bb2a566e15e3bcfcb66e8e47ff2a1
Janos Kovacs <jankovac503@gmail.com>
2013-06-13 10:56:27 +0300

    ivi-resource-manager: basic framework for screen resource

--- src/plugins/ivi-resource-manager/plugin-ivi-resource-manager.c
+++ src/plugins/ivi-resource-manager/plugin-ivi-resource-manager.c
@@ -34,11 +34,7 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-#include <murphy/common/macros.h>
-#include <murphy/common/mm.h>
-#include <murphy/common/mainloop.h>
-#include <murphy/common/msg.h>
-#include <murphy/common/transport.h>
+#include <murphy/common.h>
 #include <murphy/common/debug.h>
 #include <murphy/core/plugin.h>
 #include <murphy/core/console.h>
@@ -52,91 +48,56 @@
 #include <murphy/resource/manager-api.h>
 #include <murphy/resource/protocol.h>
 
+#include "screen.h"
 
-typedef struct {
-    mrp_plugin_t      *plugin;
-    mrp_event_watch_t *w;
-} manager_data_t;
+struct mrp_resmgr_data_s {
+    mrp_plugin_t        *plugin;
+    mrp_event_watch_t   *w;
+    mrp_resmgr_screen_t *screen;
+    mrp_htbl_t          *resources;
+};
 
 
-static void resource_event_handler(uint32_t, mrp_resource_set_t *, void *);
-
-
-
-#if 0
-static int set_default_configuration(void)
+void mrp_resmgr_insert_resource(mrp_resmgr_data_t *data,
+                                mrp_resource_t *key,
+                                void *resource)
 {
-    typedef struct {
-        const char     *name;
-        bool            share;
-        mrp_attr_def_t *attrs;
-    } resdef_t;
+    MRP_ASSERT(data && key && resource, "invalid argument");
+    MRP_ASSERT(data->resources, "uninitialised data structure");
 
-    static const char *zones[] = {
-        "driver",
-        "front-passenger",
-        "rear-left-passenger",
-        "rear-right-passenger",
-        NULL
-    };
-
-    static const char *classes[] = {
-        "implicit",
-        "player",
-        "game",
-        "phone",
-        "navigator",
-        NULL
-    };
-
-    static mrp_attr_def_t audio_attrs[] = {
-        { "role", MRP_RESOURCE_RW, mqi_string , .value.string="music" },
-        {  NULL ,        0       , mqi_unknown, .value.string=NULL    }
-    };
-
-    static resdef_t  resources[] = {
-        { "audio_playback" , true , audio_attrs  },
-        { "audio_recording", true , NULL         },
-        { "video_playback" , false, NULL         },
-        { "video_recording", false, NULL         },
-        {      NULL        , false, NULL         }
-    };
-
-    const char *name;
-    resdef_t *rdef;
-    uint32_t i;
-
-    mrp_zone_definition_create(NULL);
-
-    for (i = 0;  (name = zones[i]);  i++)
-        mrp_zone_create(name, NULL);
-
-    for (i = 0;  (name = classes[i]); i++)
-        mrp_application_class_create(name, i);
-
-    for (i = 0;  (rdef = resources + i)->name;  i++) {
-        mrp_resource_definition_create(rdef->name, rdef->share, rdef->attrs,
-                                       NULL, NULL);
-    }
-
-    return 0;
+    mrp_htbl_insert(data->resources, key, resource);
 }
-#endif
+
+void *mrp_resmgr_remove_resource(mrp_resmgr_data_t *data, mrp_resource_t *key)
+{
+    MRP_ASSERT(data && key, "invalid argument");
+    MRP_ASSERT(data->resources, "uninitialised data structure");
+
+    return mrp_htbl_remove(data->resources, key, FALSE);
+}
+
+void *mrp_resmgr_lookup_resource(mrp_resmgr_data_t *data, mrp_resource_t *key)
+{
+    MRP_ASSERT(data && key, "invalid argument");
+    MRP_ASSERT(data->resources, "uninitialised data structure");
+
+    return mrp_htbl_lookup(data->resources, key);
+}
 
 static void event_cb(mrp_event_watch_t *w, int id, mrp_msg_t *event_data,
                      void *user_data)
 {
-    mrp_plugin_t     *plugin   = (mrp_plugin_t *)user_data;
+    mrp_plugin_t      *plugin   = (mrp_plugin_t *)user_data;
 #if 0
-    mrp_plugin_arg_t *args     = plugin->args;
+    mrp_plugin_arg_t  *args     = plugin->args;
 #endif
-    manager_data_t   *data     = (manager_data_t *)plugin->data;
-    const char       *event    = mrp_get_event_name(id);
-    uint16_t          tag_inst = MRP_PLUGIN_TAG_INSTANCE;
-    uint16_t          tag_name = MRP_PLUGIN_TAG_PLUGIN;
-    const char       *inst;
-    const char       *name;
-    int               success;
+    mrp_resmgr_data_t *data     = (mrp_resmgr_data_t *)plugin->data;
+    const char        *event    = mrp_get_event_name(id);
+    uint16_t           tag_inst = MRP_PLUGIN_TAG_INSTANCE;
+    uint16_t           tag_name = MRP_PLUGIN_TAG_PLUGIN;
+    const char        *inst;
+    const char        *name;
+    int                success;
 
     MRP_UNUSED(w);
 
@@ -150,11 +111,7 @@ static void event_cb(mrp_event_watch_t *w, int id, mrp_msg_t *event_data,
                                   MRP_MSG_END);
             if (success) {
                 if (!strcmp(inst, plugin->instance)) {
-#if 0
-                    set_default_configuration();
-                    mrp_log_info("%s: built-in default configuration "
-                                 "is in use", plugin->instance);
-#endif
+                    data->screen = mrp_resmgr_screen_create(data);
                 }
             }
         } /* if PLUGIN_STARTED */
@@ -164,8 +121,8 @@ static void event_cb(mrp_event_watch_t *w, int id, mrp_msg_t *event_data,
 
 static int subscribe_events(mrp_plugin_t *plugin)
 {
-    manager_data_t   *data = (manager_data_t *)plugin->data;
-    mrp_event_mask_t  events;
+    mrp_resmgr_data_t *data = (mrp_resmgr_data_t *)plugin->data;
+    mrp_event_mask_t   events;
 
     mrp_set_named_events(&events,
                          MRP_PLUGIN_EVENT_LOADED,
@@ -184,7 +141,7 @@ static int subscribe_events(mrp_plugin_t *plugin)
 
 static void unsubscribe_events(mrp_plugin_t *plugin)
 {
-    manager_data_t *data = (manager_data_t *)plugin->data;
+    mrp_resmgr_data_t *data = (mrp_resmgr_data_t *)plugin->data;
 
     if (data->w) {
         mrp_del_event_watch(data->w);
@@ -193,16 +150,37 @@ static void unsubscribe_events(mrp_plugin_t *plugin)
 }
 
 
+static int hash_compare(const void *key1, const void *key2)
+{
+    if (key1 < key2)
+        return -1;
+    if (key1 > key2)
+        return 1;
+    return 0;
+}
+
+static uint32_t hash_function(const void *key)
+{
+    return (uint32_t)(((uint64_t)key >> 4) & 0xffffffff);
+}
+
 
 static int manager_init(mrp_plugin_t *plugin)
 {
 #if 0
-    mrp_plugin_arg_t *args = plugin->args;
+    mrp_plugin_arg_t  *args = plugin->args;
 #endif
-    manager_data_t   *data;
+    mrp_resmgr_data_t *data;
+    mrp_htbl_config_t  cfg;
 
     mrp_log_info("%s() called for IVI resource manager instance '%s'...",
                  __FUNCTION__, plugin->instance);
+
+    cfg.nentry = 256;
+    cfg.comp = hash_compare;
+    cfg.hash = hash_function;
+    cfg.free = NULL;
+    cfg.nbucket = cfg.nentry / 4;
 
     if (!(data = mrp_allocz(sizeof(*data)))) {
         mrp_log_error("Failed to allocate private data for IVI resource "
@@ -211,6 +189,7 @@ static int manager_init(mrp_plugin_t *plugin)
     }
 
     data->plugin = plugin;
+    data->resources = mrp_htbl_create(&cfg);
 
     plugin->data = data;
 
@@ -222,10 +201,16 @@ static int manager_init(mrp_plugin_t *plugin)
 
 static void manager_exit(mrp_plugin_t *plugin)
 {
+    mrp_resmgr_data_t *data;
+
     mrp_log_info("%s() called for IVI resource manager instance '%s'...",
                  __FUNCTION__, plugin->instance);
 
     unsubscribe_events(plugin);
+
+    if ((data = plugin->data)) {
+        mrp_resmgr_screen_destroy(data->screen);
+    }
 }
 
 
