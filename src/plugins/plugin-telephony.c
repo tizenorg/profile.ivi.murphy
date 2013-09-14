59d2fa435cb1f532a6dcf79907a7717051b0e89e
Krisztian Litkey <kli@iki.fi>
2013-09-14 14:11:32 +0300

    telephony: plugin arguments for resource set configuration
    The resource set the plugin controls on behalf of the
    telephony stack is now configurable using plugin arguments.
    Currently the resource set zone, class and priority as well
    as the playback and recording resource names, role,
    optionality and shareability are configurable. The policies
    are currently always set to relaxed. Adding configurability
    for this using a plugin argument is now trivial if needed.
    
    Change-Id: Ia80245c458014231fed1bb62cf3c660d091cd075

--- src/plugins/plugin-telephony.c
+++ src/plugins/plugin-telephony.c
@@ -36,18 +36,40 @@
 
 #include <murphy/common.h>
 #include <murphy/core.h>
-#include "murphy/plugins/telephony/ofono.h"
-#include "murphy/plugins/telephony/telephony.h"
 
+#include "telephony/resctl.h"
+#include "telephony/ofono.h"
+#include "telephony/telephony.h"
+
+enum {
+    ARG_ZONE,                            /* resource set zone */
+    ARG_CLASS,                           /* application class */
+    ARG_PRIORITY,                        /* priority within class */
+    ARG_PLAYBACK,                        /* playback resource config */
+    ARG_RECORDING,                       /* recording resource config */
+    ARG_ROLE,                            /* (media) role attribute */
+};
 
 mrp_plugin_t *telephony_plugin;
 
 
+
 static int telephony_init(mrp_plugin_t *plugin)
 {
-    mrp_context_t *ctx = plugin->ctx;
+    mrp_context_t    *ctx  = plugin->ctx;
+    mrp_plugin_arg_t *args = plugin->args;
+    const char       *zone = args[ARG_ZONE].str;
+    const char       *cls  = args[ARG_CLASS].str;
+    uint32_t          prio = args[ARG_PRIORITY].u32;
+    const char       *play = args[ARG_PLAYBACK].str;
+    const char       *rec  = args[ARG_RECORDING].str;
+    const char       *role = args[ARG_ROLE].str;
 
-    mrp_log_info("%s() called...", __FUNCTION__);
+    if (!resctl_config(zone, cls, prio, play, rec, role)) {
+        mrp_log_error("Failed to configure telephony resource-set.");
+
+        return FALSE;
+    }
 
     if (ctx != NULL) {
         /* use the mainloop from the plugin context */
@@ -60,8 +82,6 @@ static int telephony_init(mrp_plugin_t *plugin)
 static void telephony_exit(mrp_plugin_t *plugin)
 {
     MRP_UNUSED(plugin);
-
-    mrp_log_info("%s() called...", __FUNCTION__);
 
     tel_exit_listeners();
 }
@@ -77,11 +97,27 @@ static void telephony_exit(mrp_plugin_t *plugin)
     "The telephony plugin follows ofono DBUS activity" \
     "and updates Murphy database with telephony calls information"
 
-MRP_REGISTER_CORE_PLUGIN("telephony", \
-        TELEPHONY_VERSION, TELEPHONY_DESCRIPTION, \
-        TELEPHONY_AUTHORS, TELEPHONY_HELP, MRP_SINGLETON, \
-        telephony_init, telephony_exit,
-        NULL, 0, /* args     */
-        NULL, 0, /* exports  */
-        NULL, 0, /* imports  */
-        NULL     /* commands */ );
+#define DEFAULT_ZONE      "driver"
+#define DEFAULT_CLASS     "phone"
+#define DEFAULT_PRIORITY  1
+#define DEFAULT_PLAYBACK  "audio_playback,mandatory,shared"
+#define DEFAULT_RECORDING "audio_recording,mandatory,exclusive"
+#define DEFAULT_ROLE      "phone"
+
+static mrp_plugin_arg_t telephony_args[] = {
+    MRP_PLUGIN_ARGIDX(ARG_ZONE     , STRING, "zone"     , DEFAULT_ZONE),
+    MRP_PLUGIN_ARGIDX(ARG_CLASS    , STRING, "class"    , DEFAULT_CLASS),
+    MRP_PLUGIN_ARGIDX(ARG_PRIORITY , UINT32, "priority" , DEFAULT_PRIORITY),
+    MRP_PLUGIN_ARGIDX(ARG_PLAYBACK , STRING, "playback" , DEFAULT_PLAYBACK),
+    MRP_PLUGIN_ARGIDX(ARG_RECORDING, STRING, "recording", DEFAULT_RECORDING),
+    MRP_PLUGIN_ARGIDX(ARG_ROLE     , STRING, "role"     , DEFAULT_ROLE),
+};
+
+MRP_REGISTER_CORE_PLUGIN("telephony",
+                         TELEPHONY_VERSION, TELEPHONY_DESCRIPTION,
+                         TELEPHONY_AUTHORS, TELEPHONY_HELP, MRP_SINGLETON,
+                         telephony_init, telephony_exit,
+                         telephony_args, MRP_ARRAY_SIZE(telephony_args),
+                         NULL, 0, /* exports  */
+                         NULL, 0, /* imports  */
+                         NULL     /* commands */ );
