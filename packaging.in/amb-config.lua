4ee87ed1e6f7199e6f6df598916c2ce351d652e0
Ismo Puustinen <ismo.puustinen@intel.com>
2013-11-06 17:46:40 +0200

    packaging: listen to some more AMB properties.
    Change-Id: I22a9b96f1883556d82e1d933c2204841c26c5f92

--- packaging.in/amb-config.lua
+++ packaging.in/amb-config.lua
@@ -5,65 +5,39 @@
     the built-in handler.
 --]]
 
-
 amb.property {
     name = "vehicle_speed",
-    basic_table_name = "amb_vehicle_speed", -- default: "amb_" + name
+    basic_table_name = "amb_vehicle_speed",
     dbus_data = {
         obj = "undefined",
         interface = "org.automotive.VehicleSpeed",
         property = "VehicleSpeed",
         notification = "VehicleSpeedChanged",
+        signature = "q",
+    },
+}
+
+
+--[[
+    Gear position property
+--]]
+
+amb.property {
+    name = "gear_position",
+    basic_table_name = "amb_gear_position",
+    dbus_data = {
+        obj = "undefined",
+        interface = "org.automotive.Transmission",
+        objectname = "Transmission",
+        property = "GearPosition",
+        notification = "GearPositionChanged",
         signature = "i",
     },
 }
 
 
 --[[
-    Shift position property
-
-    Use custom handler for setting the stick status.
---]]
-
-stick_handler = function (self, property)
-    -- insert or update the incoming data
-
-    print("shift position handler: " .. property)
-
-    table = self.outputs.test_table
-
-    table:replace({ id = 0, shift_position = property })
-end
-
-amb.property {
-    name = "stick_handler",
-    handler = stick_handler,
-    outputs = {
-        test_table = mdb.table {
-            name = "amb_shift_position",
-            index = { "id" },
-            create = true,
-            columns = {
-                { "id", mdb.unsigned },
-                { "shift_position", mdb.unsigned },
-            }
-        }
-    },
-    dbus_data = {
-        obj = "undefined",
-        interface = "org.automotive.Transmission",
-        property = "ShiftPosition",
-        notification = "ShiftPositionChanged",
-        signature = "y",
-    },
-}
-
-
---[[
     Exterior brightness property
-
-    This property has a basic type which is updated often, therefore use
-    the built-in handler.
 --]]
 
 amb.property {
@@ -75,5 +49,21 @@ amb.property {
         property = "ExteriorBrightness",
         notification = "ExteriorBrightnessChanged",
         signature = "q",
+    },
+}
+
+--[[
+    Turn signal property
+--]]
+
+amb.property {
+    name = "turn_signal",
+    basic_table_name = "amb_turn_signal",
+    dbus_data = {
+        obj = "undefined",
+        interface = "org.automotive.TurnSignal",
+        property = "TurnSignal",
+        notification = "TurnSignalChanged",
+        signature = "i",
     },
 }
