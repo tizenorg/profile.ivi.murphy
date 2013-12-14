4fa281bf538862a69b4019c5e6199e279fa2b5e9
Janos Kovacs <jankovac503@gmail.com>
2013-12-14 02:19:25 +0200

    system-controller: add screen resource support
    Change-Id: I9738c71923fc7e1ed305f3ab9a1fcd79fbfea30d

--- src/plugins/system-controller/resource-manager/scripting-resource-manager.c
+++ src/plugins/system-controller/resource-manager/scripting-resource-manager.c
@@ -46,6 +46,8 @@
 
 #include "scripting-resource-manager.h"
 #include "wayland/scripting-wayland.h"
+#include "screen.h"
+#include "notifier.h"
 
 #define RESOURCE_MANAGER_CLASS   MRP_LUA_CLASS_SIMPLE(resource_manager)
 
@@ -54,6 +56,7 @@ typedef struct funcbridge_def_s    funcbridge_def_t;
 
 struct scripting_resmgr_s {
     mrp_resmgr_t *resmgr;
+    mrp_funcbridge_t *screen_event_handler;
 };
 
 struct funcbridge_def_s {
@@ -65,12 +68,13 @@ struct funcbridge_def_s {
 };
 
 static int  resmgr_create(lua_State *);
-static int  resmgr_canonical_name(lua_State *);
 static int  resmgr_getfield(lua_State *);
 static int  resmgr_setfield(lua_State *);
 static void resmgr_destroy(void *);
 
 static scripting_resmgr_t *resmgr_check(lua_State *, int);
+
+static void screen_event_handler_callback(mrp_resmgr_t *,mrp_resmgr_event_t *);
 
 static bool register_methods(lua_State *);
 
@@ -91,7 +95,7 @@ MRP_LUA_CLASS_DEF_SIMPLE (
 
 
 static mrp_funcbridge_t *area_create;
-
+static mrp_funcbridge_t *window_raise;
 
 void mrp_resmgr_scripting_init(lua_State *L)
 {
@@ -99,6 +103,8 @@ void mrp_resmgr_scripting_init(lua_State *L)
 
     mrp_lua_create_object_class(L, RESOURCE_MANAGER_CLASS);
     register_methods(L);
+
+    mrp_resmgr_scripting_notifier_init(L);
 }
 
 
@@ -133,6 +139,7 @@ static int resmgr_create(lua_State *L)
     const char *fldnam;
     scripting_resmgr_t *rm;
     int table;
+    mrp_funcbridge_t *screen_event_handler = NULL;
 
     MRP_LUA_ENTER;
 
@@ -149,6 +156,10 @@ static int resmgr_create(lua_State *L)
     MRP_LUA_FOREACH_FIELD(L, 2, fldnam, fldnamlen) {
         switch (mrp_resmgr_scripting_field_name_to_type(fldnam, fldnamlen)) {
 
+        case SCREEN_EVENT_HANDLER:
+            screen_event_handler = mrp_funcbridge_create_luafunc(L, -1);
+            break;
+
         default:
             lua_pushvalue(L, -2);
             lua_pushvalue(L, -2);
@@ -161,7 +172,12 @@ static int resmgr_create(lua_State *L)
         luaL_error(L, "can't create resmgr object");
 
     rm->resmgr = resmgr;
+    rm->screen_event_handler = screen_event_handler;
 
+    resmgr->scripting_data = rm;
+
+    mrp_resmgr_notifier_register_event_callback(resmgr,
+                                                screen_event_handler_callback);
     lua_settop(L, table);
 
     MRP_LUA_LEAVE(1);
@@ -187,6 +203,14 @@ static int resmgr_getfield(lua_State *L)
 
         case AREA_CREATE:
             mrp_funcbridge_push(L, area_create);
+            break;
+
+        case WINDOW_RAISE:
+            mrp_funcbridge_push(L, window_raise);
+            break;
+
+        case SCREEN_EVENT_HANDLER:
+            mrp_funcbridge_push(L, rm->screen_event_handler);
             break;
 
         default:
@@ -229,6 +253,69 @@ static scripting_resmgr_t *resmgr_check(lua_State *L, int idx)
 }
 
 
+static void screen_event_handler_callback(mrp_resmgr_t *resmgr,
+                                   mrp_resmgr_event_t *event)
+{
+    lua_State *L;
+    scripting_resmgr_t *rm;
+    void *sev;
+    mrp_funcbridge_value_t args[4], ret;
+    char t;
+    bool success;
+
+    MRP_ASSERT(resmgr && event, "invalid argument");
+
+    if (!(rm = resmgr->scripting_data)) {
+        mrp_debug("can't deliver resource events to LUA: "
+                  "scripting resource manager was not created yet");
+        return;
+    }
+
+    if (!(L = mrp_lua_get_lua_state())) {
+        mrp_log_error("can't deliver resource events: LUA is not initialesed");
+        return;
+    }
+
+    switch (event->type) {
+
+    case MRP_RESMGR_EVENT_SCREEN:
+        sev = mrp_resmgr_scripting_screen_event_create_from_c(L, event);
+        break;
+
+    case MRP_RESMGR_EVENT_AUDIO:
+        sev = NULL;
+        break;
+
+    case MRP_RESMGR_EVENT_INPUT:
+        sev = NULL;
+        break;
+
+    default:
+        sev = NULL;
+        break;
+    }
+
+    if (!sev) {
+        mrp_debug("can't deliver resource events to LUA: "
+                  "failed to create scripting event");
+        return;
+    }
+
+    args[0].pointer = rm;
+    args[1].pointer = sev;
+
+    memset(&ret, 0, sizeof(ret));
+
+    success = mrp_funcbridge_call_from_c(L, rm->screen_event_handler, "oo",
+                                         args, &t, &ret);
+    if (!success) {
+        mrp_log_error("failed to call resource_manager.screen_event_handler method "
+                      "(%s)", ret.string ? ret.string : "NULL");
+        mrp_free((void *)ret.string);
+    }
+}
+
+
 static bool area_create_bridge(lua_State *L,
                                void *data,
                                const char *signature,
@@ -238,6 +325,7 @@ static bool area_create_bridge(lua_State *L,
 {
     mrp_wayland_area_t *area;
     mrp_resmgr_t *resmgr;
+    const char *zonename;
 
     MRP_UNUSED(L);
     MRP_UNUSED(data);
@@ -246,8 +334,8 @@ static bool area_create_bridge(lua_State *L,
 
     *ret_type = MRP_FUNCBRIDGE_NO_DATA;
 
-    if (strcmp(signature, "oo")) {
-        mrp_log_error("bad signature: expected 'oo' got '%s'",signature);
+    if (strcmp(signature, "oos")) {
+        mrp_log_error("bad signature: expected 'oos' got '%s'",signature);
         return false;
     }
 
@@ -261,10 +349,93 @@ static bool area_create_bridge(lua_State *L,
         return false;
     }
 
+    zonename = args[2].string;
+
+    mrp_resmgr_screen_area_create(resmgr->screen, area, zonename);
 
     return true;
 }
 
+
+static bool window_raise_bridge(lua_State *L,
+                               void *data,
+                               const char *signature,
+                               mrp_funcbridge_value_t *args,
+                               char *ret_type,
+                               mrp_funcbridge_value_t *ret_val)
+{
+    mrp_resmgr_t *resmgr;
+    const char *appid;
+    int32_t surfaceid;
+    int32_t direction;
+
+    MRP_UNUSED(L);
+    MRP_UNUSED(data);
+    MRP_UNUSED(ret_val);
+    MRP_ASSERT(signature && args && ret_type, "invalid argument");
+
+    *ret_type = MRP_FUNCBRIDGE_NO_DATA;
+
+    if (strcmp(signature, "osdd")) {
+        mrp_log_error("bad signature: expected 'osdd' got '%s'", signature);
+        return false;
+    }
+
+    if (!(resmgr = mrp_resmgr_scripting_unwrap(args[0].pointer))) {
+        mrp_log_error("argument 1 is not a 'resource_manager' class object");
+        return false;
+    }
+
+    if (!(appid = args[1].string)) {
+        mrp_log_error("argument 2 is an invalid appid string");
+        return false;
+    }
+
+    if ((surfaceid = args[2].integer) < 0) {
+        mrp_log_error("argument 3 is an invalid surfaceid");
+        return false;
+    }
+
+    direction = args[3].integer;
+
+    if (direction > 0)
+        mrp_screen_resource_raise(resmgr->screen, appid, surfaceid);
+    else if (direction < 0)
+        mrp_screen_resource_lower(resmgr->screen, appid, surfaceid);
+
+    return true;
+}
+
+
+static bool register_methods(lua_State *L)
+{
+#define FUNCBRIDGE(n,s,d) { #n, s, n##_bridge, d, &n }
+#define FUNCBRIDGE_END    { NULL, NULL, NULL, NULL, NULL }
+
+    static funcbridge_def_t funcbridge_defs[] = {
+        FUNCBRIDGE(area_create   , "oos" , NULL),
+        FUNCBRIDGE(window_raise  , "osdd", NULL),
+        FUNCBRIDGE_END
+    };
+
+    mrp_funcbridge_t *f;
+    funcbridge_def_t *d;
+    bool success = true;
+
+    for (d = funcbridge_defs;   d->name;    d++) {
+        *(d->ptr) = f = mrp_funcbridge_create_cfunc(L, d->name, d->sign,
+                                                    d->func, d->data);
+        if (!f) {
+            mrp_log_error("failed to register builtin function '%s'", d->name);
+            success = false;
+        }
+    }
+
+    return success;
+
+#undef FUNCBRIDGE_END
+#undef FUNCBRIDGE
+}
 
 
 mrp_resmgr_scripting_field_t
@@ -294,8 +465,20 @@ mrp_resmgr_scripting_field_name_to_type(const char *name, ssize_t len)
     switch (len) {
 
     case 4:
-        if (!strcmp(name, "name"))
-            return NAME;
+        switch (name[0]) {
+        case 'a':
+            if (!strcmp(name, "appid"))
+                return APPID;
+            if (!strcmp(name, "area"))
+                return AREA;
+            break;
+        case 'n':
+            if (!strcmp(name, "name"))
+                return NAME;
+            break;
+        default:
+            break;
+        }
         break;
 
     case 5:
@@ -304,9 +487,21 @@ mrp_resmgr_scripting_field_name_to_type(const char *name, ssize_t len)
             if (!strcmp(name, "AUDIO"))
                 return AUDIO;
             break;
+        case 'e':
+            if (!strcmp(name, "event"))
+                return EVENT;
+            break;
         case 'i':
             if (!strcmp(name, "input"))
                 return INPUT;
+            break;
+        case 'l':
+            if (!strcmp(name, "layer"))
+                return LAYER;
+            break;
+        case 't':
+            if (!strcmp(name, "type"))
+                return TYPE;
             break;
         default:
             break;
@@ -319,8 +514,18 @@ mrp_resmgr_scripting_field_name_to_type(const char *name, ssize_t len)
         break;
 
     case 7:
-        if (!strcmp(name, "classes"))
-            return CLASSES;
+        switch (name[0]) {
+        case 'c':
+            if (!strcmp(name, "classes"))
+                return CLASSES;
+            break;
+        case 's':
+            if (!strcmp(name, "surface"))
+                return SURFACE;
+            break;
+        default:
+            break;
+        }
         break;
 
     case 9:
@@ -338,38 +543,19 @@ mrp_resmgr_scripting_field_name_to_type(const char *name, ssize_t len)
             return AREA_CREATE;
         break;
 
+    case 12:
+        if (!strcmp(name, "window_raise"))
+            return WINDOW_RAISE;
+        break;
+
+    case 20:
+        if (!strcmp(name, "screen_event_handler"))
+            return SCREEN_EVENT_HANDLER;
+        break;
+
     default:
         break;
     }
 
     return 0;
-}
-
-static bool register_methods(lua_State *L)
-{
-#define FUNCBRIDGE(n,s,d) { #n, s, n##_bridge, d, &n }
-#define FUNCBRIDGE_END    { NULL, NULL, NULL, NULL, NULL }
-
-    static funcbridge_def_t funcbridge_defs[] = {
-        FUNCBRIDGE(area_create   , "oo"  , NULL),
-        FUNCBRIDGE_END
-    };
-
-    mrp_funcbridge_t *f;
-    funcbridge_def_t *d;
-    bool success = true;
-
-    for (d = funcbridge_defs;   d->name;    d++) {
-        *(d->ptr) = f = mrp_funcbridge_create_cfunc(L, d->name, d->sign,
-                                                    d->func, d->data);
-        if (!f) {
-            mrp_log_error("failed to register builtin function '%s'", d->name);
-            success = false;
-        }
-    }
-
-    return success;
-
-#undef FUNCBRIDGE_END
-#undef FUNCBRIDGE
 }
