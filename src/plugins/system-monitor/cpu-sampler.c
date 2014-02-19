02816c73d99fd2a71cb10a5db63fd8ff3e944283
Krisztian Litkey <krisztian.litkey@intel.com>
2014-02-19 15:30:09 +0200

    system-monitor: support 'sampling' per-cgroup CPU usage/load.
    Change-Id: Iecee765c05a16b50e701146bdf7612105798230f

--- src/plugins/system-monitor/cpu-sampler.c
+++ src/plugins/system-monitor/cpu-sampler.c
@@ -32,7 +32,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <time.h>
 #include <math.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
@@ -41,6 +43,11 @@
 #include <murphy/common/debug.h>
 
 #include "cpu-sampler.h"
+#include "cgroup.h"
+
+#ifndef PATH_MAX
+#    define PATH_MAX 1024
+#endif
 
 /*
  * CPU 'ticks' (entry for a single CPU in /proc/stat)
@@ -70,11 +77,30 @@ typedef struct {
 } cpu_state_t;
 
 
-static int       statfd = -1;            /* fd for /proc/stat */
-static cpu_t    *cpus;                   /* known CPUs */
-static int       ncpu;                   /* number of CPUs */
-static uint32_t  cpu_mask;               /* mask of monitored CPUs */
-static cpu_state_t *states;              /* CPU load/state tracking */
+/*
+ * a monitored cgroup
+ */
+
+typedef struct {
+    mrp_list_hook_t  hook;               /* to list of monitored cgroups */
+    int              id;                 /* cgroup 'CPU' id */
+    cgroup_t        *cg;                 /* actual cgroup */
+    uint64_t        *samples[2];         /* sample buffer */
+    uint64_t         tstamps[2];
+    int              current;            /* current write index */
+} cpu_cgroup_t;
+
+
+static int           statfd = -1;        /* fd for /proc/stat */
+static cpu_t        *cpus;               /* known CPUs */
+static int           ncpu;               /* number of CPUs */
+static uint32_t      cpu_mask;           /* mask of monitored CPUs */
+static cpu_state_t  *states;             /* CPU load/state tracking */
+static MRP_LIST_HOOK(cgroups);           /* monitored cgroups */
+static int           cgrpid;             /* next cgroup id */
+
+
+static cpu_cgroup_t *find_cgroup_by_id(int id);
 
 
 static int enumerate_cpus(void)
@@ -82,6 +108,9 @@ static int enumerate_cpus(void)
     cpu_t *cpu;
     char   buf[4096], *p, *e;
     int    i, len;
+
+    if (ncpu > 0)
+        return ncpu;
 
     if (statfd < 0) {
         if ((statfd = open("/proc/stat", O_RDONLY)) < 0)
@@ -166,8 +195,10 @@ int cpu_get_id(const char *name)
 
 const char *cpu_get_name(int id)
 {
-    cpu_t *cpu;
-    int    i;
+    cpu_t           *cpu;
+    cpu_cgroup_t    *cgrp;
+    mrp_list_hook_t *p, *n;
+    int              i;
 
     if (cpus == NULL && enumerate_cpus() < 0)
         return NULL;
@@ -175,6 +206,13 @@ const char *cpu_get_name(int id)
     for (i = 0, cpu = cpus; i < ncpu; i++, cpu++)
         if (cpu->id == id)
             return cpu->name;
+
+    mrp_list_foreach(&cgroups, p, n) {
+        cgrp = mrp_list_entry(p, typeof(*cgrp), hook);
+
+        if (cgrp->id <= id && id < cgrp->id + ncpu)
+            return cgroup_get_name(cgrp->cg);
+    }
 
     return NULL;
 }
@@ -203,7 +241,7 @@ int cpu_set_cpu_mask(uint32_t mask)
 }
 
 
-static int sample_load(void)
+static int sample_cpus(void)
 {
     char      buf[4096], *p;
     int       len;
@@ -288,9 +326,96 @@ static int sample_load(void)
 }
 
 
+static int sample_cgroup(cpu_cgroup_t *cgrp, uint64_t tstamp)
+{
+    int rv;
+
+    cgrp->tstamps[cgrp->current] = tstamp;
+    rv = cgroup_get_cpu_usage(cgrp->cg, cgrp->samples[cgrp->current], ncpu);
+    cgrp->current = !cgrp->current;
+
+    return rv;
+}
+
+
+static int sample_cgroups(void)
+{
+    mrp_list_hook_t *p, *n;
+    cpu_cgroup_t    *cgrp;
+    struct timespec  ts;
+    uint64_t         tstamp;
+
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    tstamp = ts.tv_sec * 1000 + (ts.tv_nsec + 500) / (1000*1000);
+
+    mrp_list_foreach(&cgroups, p, n) {
+        cgrp = mrp_list_entry(p, typeof(*cgrp), hook);
+
+        if (sample_cgroup(cgrp, tstamp) < 0)
+            return -1;
+    }
+
+    return 0;
+}
+
+
 int cpu_sample_load(void)
 {
-    return sample_load();
+    if (sample_cpus() < 0 || sample_cgroups() < 0)
+        return -1;
+    else
+        return 0;
+}
+
+
+static int get_cgroup_sample(int cpu, cpu_sample_t sample)
+{
+    cpu_cgroup_t *cgrp;
+    uint64_t      diff, total;
+    int           curr, prev, idx, pcnt;
+
+    if ((cgrp = find_cgroup_by_id(cpu)) == NULL)
+        return -1;
+
+    if (sample != CPU_SAMPLE_LOAD && sample != CPU_SAMPLE_IDLE)
+        return -1;
+
+    idx = (cpu - CPU_SAMPLE_CGROUP) % ncpu;
+
+    curr = !cgrp->current;
+    prev = !curr;
+
+    diff  = (cgrp->samples[curr][idx] - cgrp->samples[prev][idx]) / (1000*1000);
+    total = cgrp->tstamps[curr] - cgrp->tstamps[prev];
+    pcnt  = (int)floor(0.5 + (100.0 * diff) / total);
+
+    /*
+     * Notes:
+     *     We want to keep our ordinary and cgroup CPU sampling
+     *     semantically as identical as possible. When sampling
+     *     CPUs we get ticks or jiffies spent in various states
+     *     per CPU so calculating the ratio of any sample to the
+     *     total will automatically result in the right figure.
+     *
+     *     However, when sampling cgroup CPU usage, we're getting
+     *     CPU usage (in nanoseconds) versus a measurement period.
+     *     Now if we have multiple CPUs or cores and we're tracking
+     *     the combined virtual CPU, we can get CPU usage up to the
+     *     period times the number of CPUs if the cgroup maxes out
+     *     all CPUs.
+     *
+     *     To get to a comparable figure, we might need to scale down
+     *     the execution time by the number of physical CPUs/cores.
+     */
+
+    if (ncpu > 1 && ((cpu - CPU_SAMPLE_CGROUP) % (ncpu - 1)) == 0)
+        pcnt /= (ncpu - 1);
+
+    if (sample == CPU_SAMPLE_LOAD)
+        return pcnt;
+    else
+        return 100 - pcnt;
 }
 
 
@@ -301,6 +426,9 @@ int cpu_get_sample(int cpu, cpu_sample_t sample)
 
     if (cpus == NULL && enumerate_cpus() < 0)
         return -1;
+
+    if (cpu >= ncpu)
+        return get_cgroup_sample(cpu, sample);
 
     if (cpu >= ncpu || !(cpu_mask & (1 << cpu))) {
         errno = ENOENT;
@@ -352,4 +480,139 @@ int cpu_get_sample(int cpu, cpu_sample_t sample)
     default:                    return -1;
 #undef PCNT
     }
+}
+
+
+static cpu_cgroup_t *find_cgroup_by_name(const char *name, cgroup_t **cgp)
+{
+    mrp_list_hook_t *p, *n;
+    cpu_cgroup_t    *cgrp;
+    cgroup_t        *cg;
+
+    if ((cg = cgroup_open(CGROUP_TYPE_CPUACCT, name, O_RDONLY)) == NULL)
+        return NULL;
+
+    mrp_list_foreach(&cgroups, p, n) {
+        cgrp = mrp_list_entry(p, typeof(*cgrp), hook);
+
+        /* note that we implicitly pass a reference to the caller here */
+        if (cgrp->cg == cg)
+            return cgrp;
+    }
+
+    if (cgp != NULL)
+        *cgp = cg;
+    else
+        cgroup_unref(cg);
+
+    return NULL;
+}
+
+
+static cpu_cgroup_t *find_cgroup_by_id(int id)
+{
+    mrp_list_hook_t *p, *n;
+    cpu_cgroup_t    *cgrp;
+
+    mrp_list_foreach(&cgroups, p, n) {
+        cgrp = mrp_list_entry(p, typeof(*cgrp), hook);
+
+        if (cgrp->id <= id && id < cgrp->id + ncpu)
+            return cgrp;
+    }
+
+    return NULL;
+}
+
+
+static void unregister_cgroup(cpu_cgroup_t *cgrp)
+{
+    if (cgrp == NULL)
+        return;
+
+    mrp_list_delete(&cgrp->hook);
+    mrp_free(cgrp->samples[0]);
+    mrp_free(cgrp->samples[1]);
+}
+
+
+static void unref_cgroup(cpu_cgroup_t *cgrp)
+{
+    if (cgrp == NULL || !cgroup_unref(cgrp->cg))
+        return;
+
+    cgrp->cg = NULL;
+    unregister_cgroup(cgrp);
+}
+
+
+int cpu_register_cgroup(const char *name)
+{
+    cpu_cgroup_t *cgrp;
+    cgroup_t     *cg;
+    int           cpuid;
+    const char   *grp, *cpu;
+    char          buf[PATH_MAX];
+
+    if (enumerate_cpus() < 0)
+        return -1;
+
+    if ((cpu = strchr(name, '#')) == NULL) {
+        grp = name;
+        cpu = "cpu";
+    }
+    else {
+        int len = cpu - name;
+
+        if (len > (int)sizeof(buf) - 1)
+            return -1;
+
+        strncpy(buf, name, len);
+        buf[len] = '\0';
+
+        grp = buf;
+        cpu++;
+    }
+
+    if ((cpuid = cpu_get_id(cpu)) < 0)
+        return -1;
+
+    if ((cgrp = find_cgroup_by_name(grp, &cg)) != NULL)
+        return cgrp->id + cpuid;
+    else if (cg == NULL)
+        return -1;
+
+    if ((cgrp = mrp_allocz(sizeof(*cgrp))) == NULL)
+        return -1;
+
+    mrp_list_init(&cgrp->hook);
+    cgrp->id = CPU_SAMPLE_CGROUP + (ncpu * cgrpid++);
+    cgrp->cg = cg;
+
+    cgrp->samples[0] = mrp_allocz(ncpu * sizeof(uint64_t));
+    cgrp->samples[1] = mrp_allocz(ncpu * sizeof(uint64_t));
+
+    if (cgrp->samples[0] == NULL || cgrp->samples[1] == NULL)
+        goto fail;
+
+    if (cgroup_get_cpu_usage(cgrp->cg, cgrp->samples[0], ncpu) < 0)
+        goto fail;
+
+    mrp_list_append(&cgroups, &cgrp->hook);
+
+    return cgrp->id;
+
+ fail:
+    if (cgrp != NULL)
+        unregister_cgroup(cgrp);
+    else
+        cgroup_unref(cg);
+
+    return -1;
+}
+
+
+void cpu_unregister_cgroup(int id)
+{
+    unref_cgroup(find_cgroup_by_id(id));
 }
